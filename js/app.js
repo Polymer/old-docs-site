98178eaa6784c2239bbab32f4f41b1e638dbd65d
Eric Bidelman <ebidel@gmail.com>
2014-01-28 07:40:05 -0800

    Remove ajax from site because dynamic imports are not working yet. Add new mapping to samples/components becuase Jekyll is leaving it out :(

--- js/app.js
+++ js/app.js
@@ -138,9 +138,9 @@ function initPage(opt_inDoc) {
 function ajaxifySite() {
   var docsMenu = document.querySelector('docs-menu');
 
-  document.addEventListener('WebComponentsReady', function(e) {
-    docsMenu.ajaxify = true;
-  });
+  // document.addEventListener('polymer-ready', function(e) {
+  //   docsMenu.ajaxify = true;
+  // });
 
   document.addEventListener('click', function(e) {
     var viableLink = false;
