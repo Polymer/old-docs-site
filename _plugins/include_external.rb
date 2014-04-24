f911437469072b8e5f58cfd8792d9e53f75856d4
Arthur Evans <arthure@google.com>
2014-04-24 14:38:47 -0700

    Folded include_code functionality into include_external

--- _plugins/include_external.rb
+++ _plugins/include_external.rb
@@ -1,14 +1,72 @@
+#
+# Includes external code. 
+# Supports including named regions from a sample file. 
+# 
+# Usage:
+#
+# {% include_external filename %}
+#
+# or:
+#
+# {% include_external filename region-name %}
+#
+# Where regions are delimited by start and end tags like this:
+#
+# // [START region-name]
+# // [END region-name]
+#
+# HTML-style comments are also supported:
+#
+# <!-- [START region-name] -->
+# <!-- [END region-name] -->
+#
+# Regions may overlap. Region start and end tags are always omitted from the 
+# included content.
+#
+# When a region is selected, the region is de-indented as if the first 
+# non-whitespace character in the start tag is column 0.
+#
+#
 module Jekyll
   class IncludeExternal < Liquid::Tag
 
-    def initialize(tag_name, text, tokens)
-      @text = text
+    def initialize(tag_name, param_string, tokens)
+      params = param_string.split(" ").map(&:strip)
+      @filename = params[0]
+      @inregion = true
+      if (params.length > 1) 
+        @region = params[1]
+        @inregion = false
+      end
+      @output = []
 
       begin
-        f = File.new(@text.strip, "r")
-        @output = f.read()
+        startpattern = /^\s*(?:\/\/|<!--)\s*\[START\s+(\w*)\s*\]/
+        endpattern = /^\s*(?:\/\/|<!--)\s*\[END\s+(\w*)\s*\]/
+        f = File.new(@filename, "r")
+        text = f.read()
+        text.each_line do |line|
+          if m = startpattern.match(line)
+            if @region && @region ==  m[1]
+              @inregion = true
+              @base_indent = line[/^ */]
+            end
+          elsif m = endpattern.match(line)
+            if @region && @region ==  m[1]
+              @inregion = false
+            end
+          else
+            if @inregion 
+              if @base_indent
+                line = line.sub(@base_indent,"")
+              end
+              @output << line
+            end
+          end
+        end
+
       rescue => e
-        @output = "<div class=\"error\">IncludeExternal error: #{e}</div>"
+        @output << "<div class=\"error\">IncludeExternal error: #{e}</div>"
       ensure
         f.close unless f.nil?
       end
@@ -17,7 +75,7 @@ module Jekyll
     end
 
     def render(context)
-      @output
+      @output.join("")
     end
   end
 end
