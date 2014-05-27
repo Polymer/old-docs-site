6f78d6f6c6995dc6072e9f9e0de34aa72fc8e018
Eric Bidelman <ebidel@gmail.com>
2014-05-27 12:06:41 -0700

    Update setup.sh to use release updater script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,5 @@
 npm install
 
-mkdir polymer-all/projects; cd polymer-all/projects
-../tools/bin/pull-all-projects.sh
-
-cd ../..
-mkdir components; cd components
-../polymer-all/tools/bin/pull-all-polymer.sh
-../polymer-all/tools/bin/pull-all-elements.sh
+./scripts/release.sh # update components, projects, etc.
 
 grunt docs
