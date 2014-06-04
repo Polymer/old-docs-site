bf3a9c4834719cd00f7f53e910b81570f53a8a43
Eric Bidelman <ebidel@gmail.com>
2014-06-04 09:31:06 -0700

    add universal ga. Part of #416

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ function injectPage(url, opt_addToHistory) {
     }
 
     // Record GA page view early; once metadata is set up and URL is updated.
-    exports._gaq.push(['_trackPageview', location.pathname]);
+    ga('send', 'pageview', location.pathname);
 
     // Update app-bar links.
     appBar.innerHTML = doc.querySelector('app-bar').innerHTML;
@@ -256,14 +256,13 @@ document.querySelector('[data-twitter-follow]').addEventListener('click', functi
 // -------------------------------------------------------------------------- //
 
 // Analytics -----
-exports._gaq = exports._gaq || [];
-exports._gaq.push(['_setAccount', 'UA-39334307-1']);
-exports._gaq.push(['_setSiteSpeedSampleRate', 50]);
-exports._gaq.push(['_trackPageview']);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ga('create', 'UA-39334307-1', 'auto');
+ga('send', 'pageview');
 // ---------------
 
 console && console.log("%cWelcome to Polymer!\n%cweb components are the <bees-knees>",
