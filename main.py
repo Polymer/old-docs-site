954fb699549b359d3f928a5748ade24e94eccfce
Rob Dodson <robdodson@google.com>
2015-01-22 17:16:26 -0800

    Tutorial redirect

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from webapp2_extras.routes import RedirectRoute
 
 app = webapp2.WSGIApplication([
   RedirectRoute('/apps/topeka/', name='topeka', redirect_to='http://polymer-topeka.appspot.com/', strict_slash=True),
-  RedirectRoute('/apps/designer/', name='designer', redirect_to='http://polymer-designer.appspot.com/', strict_slash=True)
+  RedirectRoute('/apps/designer/', name='designer', redirect_to='http://polymer-designer.appspot.com/', strict_slash=True),
+  RedirectRoute('/apps/tutorial/', name='tutorial', redirect_to='http://polymer-tut.appspot.com/', strict_slash=True)
 ], debug=False)
