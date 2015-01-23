d86c4724aa08f3839384f7b2c355279786a261c5
Rob Dodson <robdodson@google.com>
2015-01-22 16:00:09 -0800

    Add redirects for topeka and designer

---
+++ main.py
@@ -0,0 +1,7 @@
+import webapp2
+from webapp2_extras.routes import RedirectRoute
+
+app = webapp2.WSGIApplication([
+  RedirectRoute('/apps/topeka/', name='topeka', redirect_to='http://polymer-topeka.appspot.com/', strict_slash=True),
+  RedirectRoute('/apps/designer/', name='designer', redirect_to='http://polymer-designer.appspot.com/', strict_slash=True)
+], debug=False)
